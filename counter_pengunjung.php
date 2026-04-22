f5d16fb2aaeda93d896752a4cb909deeb749d2ab
i43250533-Rasendriya-Sulthan-D <i43250533@student.polije.ac.id>
2026-04-22 14:07:22 +0700

    After

--- counter_pengunjung.php
+++ counter_pengunjung.php
@@ -7,13 +7,21 @@ class Pengunjung {
         self::$jumlah++;
     }
 
+    public static function reset() {
+        self::$jumlah = 0;
+    }
 }
 
 $p1 = new Pengunjung();
 $p2 = new Pengunjung();
 $p3 = new Pengunjung();
+$p4 = new Pengunjung();
+$p5 = new Pengunjung();
 
 echo "Jumlah Pengunjung (sebelum di reset): " . Pengunjung::$jumlah . "<br>\n";
 
+Pengunjung::reset();
+
+echo "Jumlah Pengunjung (sesudah di reset): " . Pengunjung::$jumlah . "<br>\n";
 
 ?>
